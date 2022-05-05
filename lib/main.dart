91f7061f7b64a815002e7703eaad0fd661a1271a
Wujek Srujek <wujek.srujek@gmail.com>
2022-05-05 23:46:10 +0200

    Prepare demo app
    - A widget tree where the bottom-most widget needs data defined at the
      the top.
    
    If we pass data explicitly, all of the intermediate widgets need to get
    the data to pass it down. This has the following disadvantages:
    
    - All intermediate widgets need to know the data even if they don't use
      it for anything. (Unless the whole UI is described in a single build()
      method, then the data can be passed to the bottommost child, but this
      is unlikely.)
    - This doesn't work for widgets we don't own as we can't just add a
      parameter to their constructor and have them pass it to their
      children. (Unless we define the child inline, but again, if the
      hierarchy is deep we would end up with a big build().)
    - The more data we need to pass (and in a real app it is a lot), the
      more difficult to maintain the code gets. (We can pass a single object
      with many fields that grows and grows over time, but this would still
      expose a lot of data to widgets that shouldn't know about it.)
    - As we pass down data known only at runtime, we can't optimize the UI
      with const etc.
    - ...

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,62 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  final count = _getDataFromSomewhereEgStorage();
+  runApp(
+    MyApp(
+      count: count,
+    ),
+  );
 }
 
+int _getDataFromSomewhereEgStorage() => 0;
+
 class MyApp extends StatelessWidget {
-  const MyApp();
+  final int count;
+
+  const MyApp({required this.count});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       title: 'Flutter Experiments',
-      home: MyPage(),
+      home: MyPage(
+        count: count,
+      ),
     );
   }
 }
 
 class MyPage extends StatelessWidget {
-  const MyPage();
+  final int count;
+
+  const MyPage({required this.count});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('master'),
+    return Scaffold(
+      body: MyWidget(
+        count: count,
+      ),
+    );
+  }
+}
+
+class MyWidget extends StatelessWidget {
+  final int count;
+
+  const MyWidget({required this.count});
+
+  @override
+  Widget build(BuildContext context) {
+    log('Building MyWidget');
+
+    return Center(
+      child: Text(
+        '$count',
+        style: Theme.of(context).textTheme.bodyText1!.copyWith(fontSize: 128),
       ),
     );
   }
