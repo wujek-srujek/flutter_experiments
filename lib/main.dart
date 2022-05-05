0f2372b9579bdcfee25bfca07019b4ee6920b05e
Wujek Srujek <wujek.srujek@gmail.com>
2022-05-05 23:00:01 +0200

    Prepare demo app
    - A Cubit generating Colors.
    - A deep tree of widgets reacting to Cubit state changes.
    
    Even though we ignore the state and the widgets don't change when the
    Cubit emits new states, all of them are rebuilt anyway.
    
    This is because for Flutter each state change results in a new widget
    tree - the widgets are equivalent to us humans (we understand they have
    the same type and the same configuration), but to Flutter they are *not
    equal* so it needs to assume things changed and need to be rebuilt.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,9 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'color_cubit.dart';
 
 void main() {
   runApp(MyApp());
@@ -7,9 +12,12 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Experiments',
-      home: MyPage(),
+    return BlocProvider(
+      create: (context) => ColorCubit(),
+      child: MaterialApp(
+        title: 'Flutter Experiments',
+        home: MyPage(),
+      ),
     );
   }
 }
@@ -18,8 +26,75 @@ class MyPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('master'),
+      body: MyWidget(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          context.read<ColorCubit>().refreshColor();
+        },
+        child: Icon(Icons.refresh),
+      ),
+    );
+  }
+}
+
+class MyWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<ColorCubit, Color>(
+      builder: (context, color) {
+        log('BlocBuilder.builder');
+
+        return Descendant(
+          color: Colors.red,
+          tag: 'RED',
+          child: Descendant(
+            color: Colors.green,
+            tag: 'GREEN',
+            child: Descendant(
+              color: Colors.blue,
+              tag: 'BLUE',
+              child: Descendant(
+                color: Colors.yellow,
+                tag: 'INNER',
+              ),
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
+
+class Descendant extends StatelessWidget {
+  final String tag;
+  final Color color;
+  final Widget? child;
+
+  Descendant({
+    required this.tag,
+    required this.color,
+    this.child,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    log('Building $tag');
+
+    if (child == null) {
+      WidgetsBinding.instance!.addPostFrameCallback((timeStamp) {
+        log('==================================================');
+      });
+    }
+
+    return Container(
+      color: color,
+      constraints: BoxConstraints.expand(),
+      child: Padding(
+        padding: EdgeInsets.symmetric(
+          horizontal: 48,
+          vertical: 96,
+        ),
+        child: child,
       ),
     );
   }
