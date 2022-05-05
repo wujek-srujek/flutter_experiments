9263952aa799d47d59eb5ac6b6b8048edd4a754a
Wujek Srujek <wujek.srujek@gmail.com>
2022-05-05 23:45:07 +0200

    Add analysis options

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
 class MyPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return const Scaffold(
       body: Center(
         child: Text('master'),
       ),
