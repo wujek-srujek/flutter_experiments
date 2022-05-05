4f1ecd49808ba3dce0fab22f18fa83c35c248d0f
Wujek Srujek <wujek.srujek@gmail.com>
2022-05-05 18:47:13 +0200

    Add initial application files

---
+++ README.md
@@ -0,0 +1,6 @@
+# flutter_experiments
+
+A project for experimenting with various aspects of Flutter. Each branch
+focuses on a single topic and attempts to be a step-by-step trail to follow in
+order to understand the concept thoroughly. Commit messages in these branches
+normally have comments and pointers about the code.
---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Experiments',
+      home: MyPage(),
+    );
+  }
+}
+
+class MyPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('master'),
+      ),
+    );
+  }
+}
