6da6b9068e1ff441501aeb264f68aa228c16acf9
Wujek Srujek <wujek.srujek@gmail.com>
2022-05-07 14:57:36 +0200

    Cleanup part files with a post process builder
    There exists a concept of post process builders which are run after
    'normal' builders. They don't generate output but require input to work
    on.
    
    An example post processor from the build package itself is one that
    'cleans up' intermediate files. It doesn't actually delete the files
    physically; rather, it just 'deregisters' them as build output.
    (According to
    https://github.com/dart-lang
    /build/issues/3298
    it seems not be of much use as of today.)

--- packages/hello_generator/lib/src/builder.dart
+++ packages/hello_generator/lib/src/builder.dart
@@ -1,7 +1,12 @@
 import 'package:build/build.dart';
 
+import 'common_extensions.dart';
 import 'hello_part_collector_builder.dart';
 
 Builder helloCollectorBuilder(BuilderOptions options) {
   return HelloPartCollectorBuilder();
+}
+
+PostProcessBuilder partCleanup(BuilderOptions options) {
+  return const FileDeletingBuilder([helloPartExtension]);
 }
