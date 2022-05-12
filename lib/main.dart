e3bddfefd9daccfc4f3f5d2631d4fff4b9ce5e21
Wujek Srujek <wujek.srujek@gmail.com>
2022-05-12 16:55:39 +0200

    Use assets in the app
    Everything works as it should, but using assets like this is
    error-prone: we hardcode strings, it's easy to make typos etc. Let's
    change this by generating some code for asset access.

--- lib/main.dart
+++ lib/main.dart
@@ -16,14 +16,85 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyPage extends StatelessWidget {
+class MyPage extends StatefulWidget {
   const MyPage();
 
   @override
+  State<MyPage> createState() => _MyPageState();
+}
+
+class _MyPageState extends State<MyPage> {
+  late final List<DropdownMenuItem<String>> _items;
+  late String _currentImageAsset;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _items = [
+      'assets/images/red.png',
+      'assets/images/green.png',
+      'assets/images/blue.png',
+    ].map((imageAsset) {
+      return DropdownMenuItem(
+        value: imageAsset,
+        child: _RectangularAssetImage(
+          size: 20,
+          imageAsset: imageAsset,
+        ),
+      );
+    }).toList(growable: false);
+
+    _currentImageAsset = _items.first.value!;
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('master'),
+    return Scaffold(
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 64),
+            child: DropdownButton<String>(
+              items: _items,
+              onChanged: (imageAsset) {
+                setState(() {
+                  _currentImageAsset = imageAsset!;
+                });
+              },
+              value: _currentImageAsset,
+            ),
+          ),
+          _RectangularAssetImage(
+            size: 200,
+            imageAsset: _currentImageAsset,
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class _RectangularAssetImage extends StatelessWidget {
+  final double size;
+  final String imageAsset;
+
+  const _RectangularAssetImage({
+    required this.size,
+    required this.imageAsset,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: size,
+      height: size,
+      child: FittedBox(
+        child: Image.asset(
+          imageAsset,
+        ),
       ),
     );
   }
