f1122f811fafa09c978298538147d1f97caa7534
Wujek Srujek <wujek.srujek@gmail.com>
2022-05-16 23:20:45 +0200

    Implement validator generation
    It generates a separate library with extension on the classes it
    creates validators for (supports multiple classes per input file).
    
    All lints are disabled because we never know what lints the client
    package has enabled so it is virtually impossible to generate lint-free
    code for all possible clients.
    
    The code is also formatted by default due to source_gen using dart_style
    internally.
    
    The code is generated 'by hand' writing strings; it is also possible to
    use the package code_builder but it requires a lot of fiddling with
    strings anyway and results in way more code which is even less readable,
    at least for this use case. Another way would be to use a template
    engine like mustache, but let's keep things simple.

---
+++ lib/user.validator.dart
@@ -0,0 +1,68 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+// **************************************************************************
+// ValidatorGenerator
+// **************************************************************************
+
+// ignore_for_file: type=lint
+
+import 'package:validator/validator.dart';
+
+import 'user.dart';
+
+extension $UserValidatorX on User {
+  /// Validates this [User] and returns errors grouped by field name.
+  ///
+  /// If there are no errors for a given field there will be no mapping for it
+  /// in the result. If there are no errors for any field the returned map will
+  /// be empty, never `null`.
+  Map<String, List<String>> validate() {
+    return {
+      'firstName': validateFirstName(),
+      'lastName': validateLastName(),
+      'age': validateAge(),
+    }..removeWhere((fieldName, errors) => errors.isEmpty);
+  }
+
+  /// Validates this [User.firstName].
+  ///
+  /// If there are no errors the returned list will be empty, never `null`.
+  List<String> validateFirstName() {
+    return validators.validateValue(
+      firstName,
+      const [
+        RegexPattern(pattern: r'^\p{Letter}+$'),
+      ],
+    );
+  }
+
+  /// Validates this [User.lastName].
+  ///
+  /// If there are no errors the returned list will be empty, never `null`.
+  List<String> validateLastName() {
+    return validators.validateValue(
+      lastName,
+      const [
+        RegexPattern(pattern: r'^(?:\p{Letter}+ )?\p{Letter}+$'),
+      ],
+    );
+  }
+
+  /// Validates this [User.age].
+  ///
+  /// If there are no errors the returned list will be empty, never `null`.
+  List<String> validateAge() {
+    return validators.validateValue(
+      age,
+      const [
+        nonNegative,
+        IntRange(max: 150),
+      ],
+    );
+  }
+
+  /// Whether this [User] is valid.
+  ///
+  /// Call [validate] instead if detailed error information is needed.
+  bool get isValid => validate().isEmpty;
+}
--- packages/validator_generator/lib/src/validator_generator.dart
+++ packages/validator_generator/lib/src/validator_generator.dart
@@ -1,17 +1,157 @@
+import 'dart:async';
+
 import 'package:analyzer/dart/element/element.dart';
+import 'package:analyzer/dart/element/visitor.dart';
 import 'package:build/build.dart';
 import 'package:source_gen/source_gen.dart';
 import 'package:validator/validator.dart';
 
 class ValidatorGenerator extends GeneratorForAnnotation<Validated> {
   @override
+  FutureOr<String> generate(LibraryReader library, BuildStep buildStep) async {
+    final body = await super.generate(library, buildStep);
+    if (body == '') {
+      return body;
+    }
+
+    final code = StringBuffer()
+      ..write(
+        """
+// ignore_for_file: type=lint
+
+import 'package:validator/validator.dart';
+
+import '${library.element.source.shortName}';
+
+""",
+      )
+      ..writeln(body);
+
+    return code.toString();
+  }
+
+  @override
   String? generateForAnnotatedElement(
     Element element,
     ConstantReader annotation,
     BuildStep buildStep,
   ) {
-    log.info('>>> $element');
+    final ruleCollectingVisitor = _RuleCollectingVisitor();
+    element.visitChildren(ruleCollectingVisitor);
 
-    return null;
+    if (ruleCollectingVisitor.fields.isEmpty) {
+      log.warning(
+        "'${element.name}' is annotated with '@$validated' "
+        'but defines no rules, ignoring',
+      );
+
+      return null;
+    }
+
+    final typeName = element.name;
+
+    final buffer = StringBuffer()
+      ..write(
+        '''
+extension \$${typeName}ValidatorX on $typeName {
+  /// Validates this [$typeName] and returns errors grouped by field name.
+  ///
+  /// If there are no errors for a given field there will be no mapping for it
+  /// in the result. If there are no errors for any field the returned map will
+  /// be empty, never `null`.
+  Map<String, List<String>> validate() {
+    return {
+''',
+      );
+
+    for (final fieldName in ruleCollectingVisitor.fields.keys) {
+      buffer.writeln(
+        "      '$fieldName': validate${fieldName.capitalized()}(),",
+      );
+    }
+
+    buffer.write(
+      '''
+    }..removeWhere((fieldName, errors) => errors.isEmpty);
   }
+
+''',
+    );
+
+    for (final field in ruleCollectingVisitor.fields.entries) {
+      buffer.write(
+        '''
+  /// Validates this [$typeName.${field.key}].
+  ///
+  /// If there are no errors the returned list will be empty, never `null`.
+  List<String> validate${field.key.capitalized()}() {
+    return validators.validateValue(
+      ${field.key},
+      const [
+''',
+      );
+
+      for (final rule in field.value) {
+        buffer.writeln('        $rule,');
+      }
+
+      buffer.write(
+        '''
+      ],
+    );
+  }
+
+''',
+      );
+    }
+
+    buffer.write(
+      '''
+  /// Whether this [$typeName] is valid.
+  ///
+  /// Call [validate] instead if detailed error information is needed.
+  bool get isValid => validate().isEmpty;
+}''',
+    );
+
+    return buffer.toString();
+  }
+}
+
+class _RuleCollectingVisitor extends SimpleElementVisitor<void> {
+  final Map<String, List<String>> fields = {};
+
+  @override
+  void visitFieldElement(FieldElement element) {
+    final rules = element.metadata
+        .where((annotation) {
+          final annotationElement = annotation.element!;
+          if (annotationElement is ConstructorElement) {
+            // Handles '@Annotation(a: 1, b: 2)' (created with a constructor).
+            return _validationRuleChecker.isAssignableFrom(
+              annotationElement.enclosingElement,
+            );
+          } else if (annotationElement is PropertyAccessorElement) {
+            // Handles '@annotation' (saved in a const).
+            return _validationRuleChecker.isAssignableFrom(
+              annotationElement.variable.type.element!,
+            );
+          }
+
+          return false;
+        })
+        // Convert to source code snippt, drop the leading '@'.
+        .map((annotation) => annotation.toSource().substring(1))
+        .toList(growable: false);
+
+    if (rules.isNotEmpty) {
+      fields[element.name] = rules;
+    }
+  }
+}
+
+const _validationRuleChecker = TypeChecker.fromRuntime(ValidationRule<Object>);
+
+extension _StringCapitalizedX on String {
+  String capitalized() => '${this[0].toUpperCase()}${substring(1)}';
 }
