6c6dcf144b6b4cc7e3e1d27ac9c2475eb1b4e7f9
Wujek Srujek <wujek.srujek@gmail.com>
2022-06-26 14:36:20 +0200

    Add Firebase support
    Do this using the new Dart initialization with Firebase CLI and
    FlutterFire. Follow
    https://firebase.google.com/docs/flutter/setup?platform=android
    
    This will create the Firebase project, register apps and so on for you.
    
    NOTE: The configuration files (google-services.json etc.) are ignored,
    it is expected to use a dedicated project (new or existing) for this
    branch. This is required, the project won't build otherwise.
    
    NOTE: Whenever it is necessary to choose a region, the code in this
    branch assumes 'europe-west3'. This will be important later on, when
    Functions are used, so make sure to use it or update the code
    accordingly. This is done to reduce latency.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,15 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+import 'firebase_options.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const MyApp());
 }
 
@@ -23,7 +32,7 @@ class MyPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return const Scaffold(
       body: Center(
-        child: Text('master'),
+        child: Text('Firebase'),
       ),
     );
   }
