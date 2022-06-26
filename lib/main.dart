fad6be58406e57940dbdd56dde38fae59cd4a740
Wujek Srujek <wujek.srujek@gmail.com>
2022-06-26 16:26:57 +0200

    Handle notifications correctly
    Now a SnackBar is shown no matter the app state (foreground, background,
    terminated) when a notification arrives (and is clicked if the app is
    not in the foreground). It makes use of the built-in functionality for
    Flutter to easily detect whether the app was started or resumed by a
    notification click.
    
    NOTE: It may take a while (even a minute or two) for the notification to
    arrive, so have patience.

--- lib/main.dart
+++ lib/main.dart
@@ -30,7 +30,7 @@ Future<void> main() async {
 
   FirebaseMessaging.onMessage.listen((message) {
     log('>>> Foreground message received');
-    _messageHandler(message);
+    _showSnackbarForNotification('Foreground', message);
   });
   FirebaseMessaging.onBackgroundMessage(_backgroundMessageHandler);
 
@@ -39,19 +39,21 @@ Future<void> main() async {
 
 Future<void> _backgroundMessageHandler(RemoteMessage message) async {
   log('>>> Background message received');
-  _messageHandler(message);
+  // No UI but can execute API calls etc.
 }
 
-void _messageHandler(RemoteMessage message) {
+// Used to handle the message when the UI is available. It is used either when
+// the app is in the foreground or when the app is launched by clicking on the
+// notification and using FirebaseMesseging handling.
+void _showSnackbarForNotification(String appState, RemoteMessage message) {
+  log('>>> App state: $appState');
   log('>>> Message map: ${message.toMap()}');
 
   final messageData = message.data['message'] as String?;
   if (messageData != null) {
-    // If there is no context yet (the message arrived before the UI was built)
-    // we don't show anything, which is OK for this experiment.
-    _scaffoldMessengerKey.currentState?.showSnackBar(
+    _scaffoldMessengerKey.currentState!.showSnackBar(
       SnackBar(
-        content: Text(messageData),
+        content: Text('($appState) $messageData'),
       ),
     );
   }
@@ -77,8 +79,20 @@ void _saveFcmToken(String fcmToken) {
   );
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp();
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  void initState() {
+    super.initState();
+
+    _setupBackgroundMessageHandling();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -87,6 +101,17 @@ class MyApp extends StatelessWidget {
       scaffoldMessengerKey: _scaffoldMessengerKey,
       home: MyPage(),
     );
+  }
+
+  Future<void> _setupBackgroundMessageHandling() async {
+    final initialMessage = await FirebaseMessaging.instance.getInitialMessage();
+    if (initialMessage != null) {
+      _showSnackbarForNotification('Terminated', initialMessage);
+    }
+
+    FirebaseMessaging.onMessageOpenedApp.listen((remoteMessage) {
+      _showSnackbarForNotification('Background', remoteMessage);
+    });
   }
 }
 
